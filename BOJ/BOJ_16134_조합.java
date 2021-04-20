1ac6b39b00d342ffa33a9dd65e4b00efb48f68e9
hanihn309 <hanihn309@gmail.com>
2021-04-20 09:23:57 +0900

    hanihn309 solved BOJ 16134

---
+++ BOJ/BOJ_16134_조합.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_16134_조합 { // 210420 _ 페르마의 소정리
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		
+		int P = 1000000007;
+		
+		System.out.println(nCr(N, R, P));
+	}
+
+	private static long nCr(int n, int r, int p) {
+		if(r == 0)
+			return 1L;
+		
+		long[] factorial = new long[n+1];
+		factorial[0] = 1;
+		
+		for(int i = 1; i <= n; i++)
+			factorial[i] = factorial[i-1] * i % p;
+		
+		
+		return (factorial[n] *
+				power(factorial[r], p-2, p) % p *
+				power(factorial[n-r], p-2, p) % p)
+				%p;
+	}
+
+	private static long power(long x, long y, long p) {
+		long res = 1L;
+		x %= p;
+		
+		while(y > 0) {
+			if (y % 2 != 0)
+				res = (res * x) % p;
+			
+			y = y >> 1;
+			x = (x * x) % p;
+		}
+		
+		return res;
+	}
+}
